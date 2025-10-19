1b220a94c61b6ffa217e135c627ec17026620771
aleksandrzuravl <st129760@student.spbu.ru>
2025-10-19 18:48:32 +0300

    Create Endings.h

---
+++ include/Endings.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+class BadEnding {
+public:
+    static void playEnding();
+};
+
+class GoodEnding {
+public:
+    static void playEnding();
+};
+
+class NeutralEnding {
+public:
+    static void playEnding();
+};
